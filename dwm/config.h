4010b411064c5d3315fc5b5bb343be8e09bce0a3
Frank <101807917+frankischilling@users.noreply.github.com>
2024-02-26 15:53:56 -0500

    vol fix + slstatus vol

--- dwm/config.h
+++ dwm/config.h
@@ -67,9 +67,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *mutecmd[] = { "amixer", "-D", "pulse", "sset", "0%", NULL };
-static const char *volupcmd[] = { "amixer", "-D", "pulse", "sset", "5%+", NULL };
-static const char *voldowncmd[] = { "amixer", "-D", "pulse", "sset", "5%-", "unmute", NULL };
+static const char *mutecmd[] = { "amixer", "-D", "pulse", "sset", "Master",  "0%", NULL };
+static const char *volupcmd[] = { "amixer", "-D", "pulse", "sset", "Master",  "5%+", NULL };
+static const char *voldowncmd[] = { "amixer", "-D", "pulse", "sset", "Master", "5%-", NULL };
 static const char *retromcmd[] = { "cool-retro-term", NULL};
 static const char *chromiumcmd[] = { "google-chrome-stable", NULL};
 
