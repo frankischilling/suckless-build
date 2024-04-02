7989755246827352c516c99df3169f90ba2f842f
Frank <101807917+frankischilling@users.noreply.github.com>
2024-04-02 15:47:03 -0400

    new patch

--- st/config.def.h
+++ st/config.def.h
@@ -208,6 +208,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_Escape,      keyboard_select,{ 0 } },
 };
 
 /*
